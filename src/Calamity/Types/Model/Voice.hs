e041814e8b59b3d355f7709a94ff0db3abbbbce4
Kaptch <kaptch@gmail.com>
2020-07-30 17:10:57 +0300

    voice server type

--- src/Calamity/Types/Model/Voice.hs
+++ src/Calamity/Types/Model/Voice.hs
@@ -1,7 +1,9 @@
 -- | Voice data types
 module Calamity.Types.Model.Voice
     ( module Calamity.Types.Model.Voice.VoiceState
-    , module Calamity.Types.Model.Voice.VoiceRegion ) where
+    , module Calamity.Types.Model.Voice.VoiceRegion
+    , module Calamity.Types.Model.Voice.VoiceServer ) where
 
 import           Calamity.Types.Model.Voice.VoiceRegion
+import           Calamity.Types.Model.Voice.VoiceServer
 import           Calamity.Types.Model.Voice.VoiceState
